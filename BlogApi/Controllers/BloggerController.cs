05bbc32b3d77027fdd7125488820d5db30d6f521
zsolti01 <vecseizs@kkszki.hu>
2026-09-09 10:12:40 +0200

    MySqlConnector package was added.

---
+++ BlogApi/Controllers/BloggerController.cs
@@ -0,0 +1,35 @@
+﻿using BlogApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BloggerController : ControllerBase
+    {
+        [HttpGet]
+        public List<Blogger> GetAllBlogger()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public object AddNewBlogger(Blogger blogger)
+        {
+            return null;
+        }
+
+        [HttpPut]
+        public object UpdateBlogger(int id, Blogger blogger)
+        {
+            return null;
+        }
+
+        [HttpDelete]
+        public object DeleteBlogger(int id, Blogger blogger)
+        {
+            return null;
+        }
+    }
+}
