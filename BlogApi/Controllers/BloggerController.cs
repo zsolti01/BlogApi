5993c1641d2b74eaf0f2840b22730c7c7a662a9a
zsolti01 <vecseizs@kkszki.hu>
2026-09-09 11:04:32 +0200

    Get endpoint is ready to use

--- BlogApi/Controllers/BloggerController.cs
+++ BlogApi/Controllers/BloggerController.cs
@@ -8,10 +8,40 @@ namespace BlogApi.Controllers
     [ApiController]
     public class BloggerController : ControllerBase
     {
+        private readonly string ConnectionString = "server=localhost;database=blog;uid=root;password=";
+
         [HttpGet]
         public List<Blogger> GetAllBlogger()
         {
-            return null;
+            List<Blogger> bloggers = new();
+
+            var conn = new MySqlConnector.MySqlConnection(ConnectionString);
+
+            conn.Open();
+
+            string sql = "SELECT * FROM blogger";
+
+            var cmd = new MySqlConnector.MySqlCommand(sql, conn);
+
+            var dataReader = cmd.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                var blogger = new Blogger
+                {
+                    Id = dataReader.GetInt32(0),
+                    Name = dataReader.GetString(1),
+                    Email = dataReader.GetString(2),
+                    Age = dataReader.GetInt32(3),
+                    Password = dataReader.GetString(4),
+                    RegTime = dataReader.GetDateTime(5)
+                };
+                bloggers.Add(blogger);
+            }
+
+            conn.Close();
+
+            return bloggers;
         }
 
         [HttpPost]
